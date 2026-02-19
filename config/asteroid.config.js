6c86e2a125e435563574cefb105bf3af26e4533f
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-02-19 10:40:20 -0300

    Add AsteroidConfig class to define asteroid properties such as speed, radius, color, and far distance

---
+++ config/asteroid.config.js
@@ -0,0 +1,7 @@
+export class AsteroidConfig {
+  static get speed() { return 0.5 }
+  static get radius() { return 0.5 }
+  static get color() { return 0xffffff }
+
+  static get far() { return 100 }
+}
