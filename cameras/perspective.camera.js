68eb649d7035af2d437bbc5e5efe1d594e28278b
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-02-19 00:59:34 +0000

    Add cameras

---
+++ cameras/perspective.camera.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three';
+
+export class PerspectiveCamera extends THREE.PerspectiveCamera {
+  constructor({ } = {}) {
+    super(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.position.z = 5;
+  }
+}
