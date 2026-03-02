9c5e7ed9a63f7a7064f7fa008e089c57c092a3d1
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-02 15:56:56 +0000

    Fix events

--- events/game.win.event.js
+++ events/game.win.event.js
@@ -1,6 +1,8 @@
 export class GameWinEvent extends CustomEvent {
+  static NAME = 'game.win'
+
   constructor({ points, lives } = {}) {
-    super()
+    super(GameWinEvent.NAME, { detail: { points, lives } })
     this.points = points
     this.lives = lives
   }
