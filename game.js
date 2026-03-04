e5a285c3782d028288ea5fe4c86b76b609afc3e7
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-04 11:50:23 -0300

    feat: add debug mode

--- game.js
+++ game.js
@@ -36,6 +36,7 @@ export class Game {
   asteroidInterval = null;
 
   constructor(width, height) {
+    consolee.log('Game.constructor', { width, height })
     this.scene.add(this.group);
     this.group.add(this.light);
     this.group.add(this.camera);
@@ -46,41 +47,51 @@ export class Game {
   }
 
   setWindowEvents() {
+    consolee.log('Game.setWindowEvents', {})
+
     window.addEventListener('resize', () => {
+      consolee.log('Game.setWindowEvents.resize', {})
       this.camera.aspect = window.innerWidth / window.innerHeight;
       this.camera.updateProjectionMatrix();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
     });
 
     window.addEventListener(GameOverEvent.NAME, () => {
+      consolee.log('Game.setWindowEvents.GameOverEvent', {})
       alert('Game over! ' + this.score.toString());
       this.reset();
     })
 
     window.addEventListener(GameWinEvent.NAME, () => {
+      consolee.log('Game.setWindowEvents.GameWinEvent', {})
       alert('You won! ' + this.score.toString());
       this.reset();
     })
 
     window.addEventListener(PlayerShotEvent.NAME, (event) => {
+      consolee.log('Game.setWindowEvents.PlayerShotEvent', { event })
       this.group.add(event.detail.bullet);
     });
 
     window.addEventListener(BulletOutsideEvent.NAME, (event) => {
+      consolee.log('Game.setWindowEvents.BulletOutsideEvent', { event })
       this.removeBullet(event.detail.bullet);
     });
 
     window.addEventListener(AsteroidCreatedEvent.NAME, (event) => {
+      consolee.log('Game.setWindowEvents.AsteroidCreatedEvent', { event })
       this.group.add(event.detail.asteroid);
     });
 
     window.addEventListener(PlayerCollisionEvent.NAME, (event) => {
+      consolee.log('Game.setWindowEvents.PlayerCollisionEvent', { event })
       this.removeAsteroid(event.detail.asteroid);
       this.score.subtractLife();
       this.checkGameOver();
     });
 
     window.addEventListener(AsteroidBulletCollisionEvent.NAME, (event) => {
+      consolee.log('Game.setWindowEvents.AsteroidBulletCollisionEvent', { event })
       const { asteroid, bullet } = event.detail;
       this.removeAsteroid(asteroid);
       this.removeBullet(bullet);
@@ -89,11 +100,14 @@ export class Game {
     });
 
     window.addEventListener(AsteroidOutsideEvent.NAME, (event) => {
+      consolee.log('Game.setWindowEvents.AsteroidOutsideEvent', { event })
       this.removeAsteroid(event.detail.asteroid);
     });
   }
 
   runPlayer(action, startStop) {
+    consolee.log('Game.runPlayer', { action, startStop })
+
     switch (action) {
       case 'left': startStop ? this.player.startRotateLeft() : this.player.stopRotateLeft(); break;
       case 'right': startStop ? this.player.startRotateRight() : this.player.stopRotateRight(); break;
@@ -106,6 +120,7 @@ export class Game {
   setKeyboardEvents() {
     ['keydown', 'keyup'].map(event_name => {
       window.addEventListener(event_name, (event) => {
+        consolee.log('Game.setKeyboardEvents', { event_name, event })
         event.preventDefault();
         const isKeyDown = event_name === 'keydown' ? 1 : 0;
         switch (event.key) {
@@ -120,16 +135,19 @@ export class Game {
   }
 
   removeAsteroid(asteroid) {
+    consolee.log('Game.removeAsteroid', { asteroid })
     asteroid.stop();
     this.group.remove(asteroid);
   }
 
   removeBullet(bullet) {
+    consolee.log('Game.removeBullet', { bullet })
     bullet.stop();
     this.group.remove(bullet);
   }
 
   checkGameOver() {
+    consolee.log('Game.checkGameOver', {})
     switch (true) {
       case this.score.getLives() <= 0: {
         this.stop();
@@ -143,10 +161,12 @@ export class Game {
   }
 
   enableAIControls() {
+    consolee.log('Game.enableAIControls', {})
     this.score.setIsAI(true);
   }
 
   setPlayerType(type) {
+    consolee.log('Game.setPlayerType', { type })
     this.isAI = type === NeuralNetwork.NAME;
 
     if (this.isAI) {
@@ -161,6 +181,7 @@ export class Game {
   }
 
   start() {
+    consolee.log('Game.start', {})
     this.neural_networks.push(this.createNeuralNetwork())
     this.score.setGeneration(this.neural_networks.length)
 
@@ -169,6 +190,7 @@ export class Game {
   }
 
   addAsteroid() {
+    consolee.log('Game.addAsteroid', {})
     if (this.getAsteroids().length < Game.MAX_ASTEROIDS) {
       const asteroid = new AsteroidMesh({
         getPlayer: () => this.player,
@@ -179,6 +201,7 @@ export class Game {
   }
 
   addBullet() {
+    consolee.log('Game.addBullet', {})
     const bullet = new BulletMesh({
       x: this.player.position.x,
       y: this.player.position.y,
@@ -189,13 +212,19 @@ export class Game {
     window.dispatchEvent(new PlayerShotEvent({ player: this.player, bullet }));
   }
 
-  stop() { clearInterval(this.asteroidInterval); }
+  stop() {
+    consolee.log('Game.stop', {})
+    clearInterval(this.asteroidInterval);
+  }
 
   getNeuralNetwork() {
+    consolee.log('Game.getNeuralNetwork', {})
     return this.neural_networks[this.neural_networks.length - 1]
   }
 
   runWithAI() {
+    consolee.log('Game.runWithAI', {})
+
     const input = Array.from(Array(8)).map((_, i) => this.player.getSensorData(i));
 
     const output = this.getNeuralNetwork().activate(input).map((value) => value >= 0.5);
@@ -210,6 +239,7 @@ export class Game {
   }
 
   update() {
+    consolee.log('Game.update', {})
     if (this.isAI) this.runWithAI();
 
     this.renderer.render(this.scene, this.camera);
@@ -217,6 +247,7 @@ export class Game {
   }
 
   reset() {
+    consolee.log('Game.reset', {})
     this.score.reset();
     this.player.stop();
     this.player.resetPosition().resetRotation();
@@ -226,14 +257,17 @@ export class Game {
   }
 
   getAsteroids() {
+    consolee.log('Game.getAsteroids', {})
     return this.group.children.filter((asteroid) => asteroid instanceof AsteroidMesh);
   }
 
   getBullets() {
+    consolee.log('Game.getBullets', {})
     return this.group.children.filter((bullet) => bullet instanceof BulletMesh);
   }
 
   removeAllAsteroids() {
+    consolee.log('Game.removeAllAsteroids', {})
     this.getAsteroids().map((asteroid) => {
       asteroid.stop();
       this.group.remove(asteroid);
@@ -241,6 +275,7 @@ export class Game {
   }
 
   removeAllBullets() {
+    consolee.log('Game.removeAllBullets', {})
     this.getBullets().map((bullet) => {
       bullet.stop();
       this.group.remove(bullet);
@@ -248,6 +283,7 @@ export class Game {
   }
 
   createNeuralNetwork() {
+    consolee.log('Game.createNeuralNetwork', {})
     const inputLayer = new synaptic.Layer(8);
     const hiddenLayer1 = new synaptic.Layer(10);
     const hiddenLayer2 = new synaptic.Layer(10);
